e371ba130b965587cbbed2d86699648ec582fa3d
Nadezhda Krutova <nadiya-yand@yandex.ru>
2022-10-25 21:00:29 +0300

    server-client interaction

---
+++ src/main/java/Server.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private static final int PORT = 8080;
+
+    public static void main(String[] args) {
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            System.out.println("Server started");
+
+            //           while (true) {
+            try (Socket clientSocket = serverSocket.accept()) {
+                PrintWriter out = new PrintWriter(
+                        clientSocket.getOutputStream(), true);
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+                System.out.println("New connection accepted");
+
+                final String name = in.readLine();
+
+                System.out.printf("Hi, %s, your port is %d%n",
+                        name, clientSocket.getPort());
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //                        }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
